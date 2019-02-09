8720454897c59a261bfd6fbf3869896567f3109d
przemo904 <przemosoft904@gmail.com>
2019-02-09 17:49:13 +0100

    changes

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -12,17 +12,102 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+
 
 namespace TicTacToe
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+    public enum Player {
+        X,O
+    }
+
+    
+
     public partial class MainWindow : Window
     {
+        public Player CurrentPlayer;
+
         public MainWindow()
         {
             InitializeComponent();
+            CurrentPlayer = Player.X;
+
         }
+
+
+        private void ButtonClick(object sender,RoutedEventArgs e) {
+            var bt = (Button)sender;
+            if (bt.Content == null) {
+
+                if (CurrentPlayer == Player.X) {
+                    bt.Content = "X";
+                    
+                }
+
+                if (CurrentPlayer == Player.O)
+                {
+                    bt.Content = "O";
+                }
+
+
+               CheckVictory();
+               ChangePlayer();
+            }
+        }
+        public void ChangePlayer() {
+
+            if (CurrentPlayer == Player.X)
+            {
+                CurrentPlayer = Player.O;
+                ToggleO();
+
+            }
+            else {
+                CurrentPlayer = Player.X;
+                ToggleX();
+
+
+            }
+
+        }
+
+
+        public void CheckVictory() { 
+
+
+        }
+
+
+        private void PlayerClick(object sender, RoutedEventArgs e) {
+            var bt = (ToggleButton)sender;
+            if (bt.Content.ToString() == "X") {
+                CurrentPlayer = Player.X;
+                ToggleX();
+            }
+            else
+            {
+                CurrentPlayer = Player.O;
+                ToggleO();
+
+            }
+
+        }
+
+        private void ToggleX() {
+            playerX.IsChecked = true;
+            playerO.IsChecked = false;
+        }
+        private void ToggleO()
+        {
+            playerX.IsChecked = false;
+            playerO.IsChecked = true;
+        }
+
+        private void ReplayClick(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
     }
 }
